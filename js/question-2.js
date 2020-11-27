5d3f253bdbe5b07bc6544dd912e14083461ee308
Kstavem <kstavem@gmail.com>
2020-11-27 11:27:41 +0100

    jk, it's finished now

--- js/question-2.js
+++ js/question-2.js
@@ -1,6 +1,4 @@
 // Question 2
-console.log("Question 2:")
-
 const gamesAPI = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 const gamesContainer = document.querySelector(".container");
 const header = document.querySelector("h1");
